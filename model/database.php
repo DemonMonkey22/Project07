fa227a645e18932aea28496f0f6949956f66a48f
DemonMonkey22 <erpemberton@mail.fhsu.edu>
2020-04-08 20:35:44 -0500

    fixed database?

--- model/database.php
+++ model/database.php
@@ -5,18 +5,18 @@
     //$password = 'pa55word';
 
     // Heroku connection
-    /*
-    $dsn = 'mysql:host=AVeryLongURLprovidedforJawsDBhost;dbname=YourJawsDBdbname';
-    $username = 'Your JawsDB username';
-    $password = 'Your JawsDB password';
-    */
+    
+    $dsn = 'mysql:host=o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=jkftejpk988kvek';
+    $username = 'jkftejpk988kvekk';
+    $password = 'w37tp6u8t2qys6bo';
+    
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
         $db = new PDO($dsn, $username);
 
         // Heroku connection
-        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
